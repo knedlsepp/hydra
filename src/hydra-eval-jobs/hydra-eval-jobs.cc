c5ff646b0ade6dd93ca35951f8c621dd5889f5d9
Josef Kemetmüller <josef.kemetmueller@ims.co.at>
2020-04-04 08:17:54 +0200

    HACK: Don't restrict evaluation

--- src/hydra-eval-jobs/hydra-eval-jobs.cc
+++ src/hydra-eval-jobs/hydra-eval-jobs.cc
@@ -238,7 +238,7 @@ int main(int argc, char * * argv)
 
         /* Prevent access to paths outside of the Nix search path and
            to the environment. */
-        evalSettings.restrictEval = true;
+        evalSettings.restrictEval = false;
 
         if (myArgs.dryRun) settings.readOnlyMode = true;
 
